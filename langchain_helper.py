56b32191348ca1bde19a30a8cd3b9f2dd250bea4
AnantUpadhyay02 <amanupadhyay1719@gmail.com>
2024-08-15 20:39:39 +0530

    POC application

---
+++ langchain_helper.py
@@ -0,0 +1,5 @@
+def generate_business_name(business):
+    return {
+        "business_name":"Henry's bakery",
+        "products":{"bread","cake","cookies"}
+    }
---
+++ proof_of_concept_APP.py
@@ -0,0 +1,17 @@
+#here we are using streamlit library for POC app 
+import streamlit as st
+from langchain_helper import generate_business_name
+st.title("Business Name Generator")
+
+business = st.sidebar.selectbox("pick a business",("Chinese Restaurant", "Gas Station", "Car Dealership", "Clothing Store", "Bakery", "Pharmacy", "Gym", "Coffee Shop", "Law Firm", "Consulting Agency", "Florist", "Real Estate Agency", "Bookstore", "IT Services Company", "Landscaping Service", "Automotive Repair Shop", "Dental Clinic", "Construction Company", "Online Retailer", "Event Planning Service"))
+
+
+
+
+if business:
+    response = generate_business_name(business)
+    st.header(response['business_name'])
+    products = response['products']
+    st.write("**Products that we are selling**")
+    for item in products:
+        st.write("->",item)
